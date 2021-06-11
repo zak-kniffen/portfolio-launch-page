acf11d451a8af2c3ba1b4d88ba9de31dbc0775f2
Zachary A Kniffen <zacharyakniffen@gmail.com>
2021-06-10 22:01:07 -0500

    added SVG graphic with labels

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import triforce from './clipart1751770.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
+        <div className="container">
+        <svg id="triangle" viewBox="0 0 100 100">
+            	<a href="https://www.google.com/"><polygon points="50 15, 75 50, 25 50" fill="#f0f00f"/></a>
+              <a href="https://www.bing.com/"><polygon points="0 85, 25 50, 50 85" fill="#f0f00f"/></a>
+              <a href="https://duckduckgo.com/"><polygon points="50 85, 75 50, 100 85" fill="#f0f00f"/></a>
+              <a href="https://www.google.com/"><text x="38" y="45" className="small">JAVA</text></a>
+              <a href="https://www.bing.com/"><text x="10" y="80" className="small">MERN</text></a>
+              <a href="https://duckduckgo.com/"><text x="60" y="80" className="small">MEAN</text></a>
+            
+        </svg>
+     
+     </div>   
+
       </header>
     </div>
   );
