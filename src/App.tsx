25c9a74364027904d39581c61a26d093fcf63fe2
Zachary A Kniffen <zacharyakniffen@gmail.com>
2021-06-20 21:58:10 -0500

    added border and font to triforce, added bushes

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div id="flexTop">
+      <p className="image"> </p>
+      <p className="image"> </p>
+      <p className="image"> </p>
+      <p className="image"> </p>
+      <p className="image"> </p>
+      <p className="image"> </p>
+      <p className="image"> </p>
+      </div>
       <header className="App-header">
         
 
         <div className="container">
         <svg id="triangle" viewBox="0 0 100 100">
-            	<a href="https://www.google.com/"><polygon points="50 15, 75 50, 25 50" fill="#f0f00f"/></a>
-              <a href="https://www.bing.com/"><polygon points="0 85, 25 50, 50 85" fill="#f0f00f"/></a>
-              <a href="https://duckduckgo.com/"><polygon points="50 85, 75 50, 100 85" fill="#f0f00f"/></a>
+            	<a href="https://www.google.com/"><polygon points="50 15, 75 50, 25 50" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
+              <a href="https://www.bing.com/"><polygon points="0 85, 25 50, 50 85" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
+              <a href="https://duckduckgo.com/"><polygon points="50 85, 75 50, 100 85" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
               <a href="https://www.google.com/"><text x="38" y="45" className="small">JAVA</text></a>
               <a href="https://www.bing.com/"><text x="10" y="80" className="small">MERN</text></a>
               <a href="https://duckduckgo.com/"><text x="60" y="80" className="small">MEAN</text></a>
@@ -23,6 +32,9 @@ function App() {
      </div>   
 
       </header>
+      
+  
+
     </div>
   );
 }
