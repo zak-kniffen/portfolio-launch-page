b95549181714c9228e54973ed6b56e86c6c729c0
Zachary A Kniffen <zacharyakniffen@gmail.com>
2021-06-23 05:33:19 -0500

    testing github pages build

--- src/App.tsx
+++ src/App.tsx
@@ -51,10 +51,10 @@ function App() {
 
         <div className="container">
         <svg id="triangle" viewBox="0 0 100 100">
-            	<a href="https://www.google.com/"><polygon points="50 15, 75 50, 25 50" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
+            	<a href="https://arcane-badlands-41044.herokuapp.com"><polygon points="50 15, 75 50, 25 50" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
               <a href="https://www.bing.com/"><polygon points="0 85, 25 50, 50 85" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
               <a href="https://duckduckgo.com/"><polygon points="50 85, 75 50, 100 85" fill="#f0f00f" stroke="#cebe05" strokeWidth="2"/></a>
-              <a href="https://www.google.com/"><text x="38" y="45" className="small">JAVA</text></a>
+              <a href="https://arcane-badlands-41044.herokuapp.com"><text x="38" y="45" className="small">JAVA</text></a>
               <a href="https://www.bing.com/"><text x="10" y="80" className="small">MERN</text></a>
               <a href="https://duckduckgo.com/"><text x="60" y="80" className="small">MEAN</text></a>
             
